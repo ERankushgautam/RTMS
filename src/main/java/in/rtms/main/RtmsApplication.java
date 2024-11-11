d1064fc9416ca5f1a7899f6993d929c524c4c11f
ERhoneydhiman <mhoneyhiman@gmail.com>
2024-11-11 11:08:45 +0530

    project created

---
+++ src/main/java/in/rtms/main/RtmsApplication.java
@@ -0,0 +1,13 @@
+package in.rtms.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtmsApplication.class, args);
+	}
+
+}
